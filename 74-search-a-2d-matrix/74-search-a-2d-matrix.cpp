25981525c9c9e7331595e0c52dcdf9d472235e0b
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-04-16 23:41:46 +0530

    Time: 10 ms (18.55%), Space: 9.5 MB (79.30%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    /*
+    
+    Time Complexity : O(n) + O(log(n)) ==> O(n)
+    Space Complexity : O(n) ---> n is the number of columns
+    
+    */
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int cols = matrix[0].size(); //1
+        int rows = matrix.size(); // 2
+        int whichRowanta = 0; //0
+        
+        /*
+        i = 0 
+        1 < mat[0][0] ==> 1 < 1
+        
+        */
+        
+        for(int i = 0; i < rows ; i++)
+        {
+            if(target <= matrix[i][cols - 1])
+            {
+                whichRowanta = i;
+                break;
+            }
+        }
+        
+        vector<int> search = matrix[whichRowanta];
+        
+        int s = 0;
+        int e = cols - 1;
+        bool found = false;
+        while(s <=e && !found)
+        {
+            int mid = (s) + ((e - s) / 2);
+            
+            if(search[mid] == target)
+            {
+                found = true;                
+            }
+            
+            else if(target > search[mid])
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
+        return found;
+    }
+};
