960eee62aec238421039b292c379aacf237c126e
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-05-01 10:30:45 +0530

    Time: 7 ms (97.33%), Space: 10.9 MB (45.25%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,8 +3,40 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+         /*
+            BRUTE FORCE : 
+            Time complexity : O(n*n)
+            Space Complexity : O(n)
+            vector<int> v;
+            int s = nums.size();
+            for(int i = 0 ; i < s ; i++)
+            {
+
+                for(int j = i + 1 ; j < s ; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        v.push_back(i);
+                        v.push_back(j);
+                        return v;
+
+                    }
+                }
+            }
+
+            return {};
+        }
+            
+        */
+        
         
         //Optimised Approach is usage of hashMap
+        /*
+        
+        Time complexity : O(n)
+        Space Complexity : O(n)
+        
+        */
         unordered_map<int, int> myMap;
         
         int temp;
