6b32532b2c44cf1c1c359f93bc721fd570b49084
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-05-02 20:55:27 +0530

    Time: 53 ms (38.42%), Space: 14.2 MB (15.08%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,33 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+        
+        '''
+        
+        
+            Time Complexity : O(n)
+            Space Complexity : O(n)
+            
+        '''
+        
+        s = s.lstrip(" ")
+        
+        s = s.rstrip(" ")
+        
+        l = list((s).split(" "))
+        
+        ans = ""
+        
+        
+        
+        n = len(l) - 1;
+        while(n >=0):         
+            if(l[n] != '' and l[n] != " "):
+       
+                ans = ans + l[n] + " ";
+            n = n - 1;
+            continue
+        
+
+        return ans[:len(ans) - 1];
+        
