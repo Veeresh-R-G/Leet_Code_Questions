0f0018b1d9d795a716473c758704bdbc4a968216
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-05-08 21:04:48 +0530

    Time: 5 ms (14.49%), Space: 6.8 MB (55.40%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -16,15 +16,49 @@ public:
         the reason we are using the second queue is two store the incoming element
         
         and get it at the front of the queue to mimic the FILO property of the stack
+        
+        
+        2. Using 1 Queue
+        
+        q1
+        
+        push into q1
+        
+        if q1 is not empty
+        
+        calculate size of q1 
+        
+        for size - 1 times take the element from front and push it back
+        
     */
     
     queue<int> q1;
     queue<int> q2;
+    
+    queue<int> q3;
     MyStack() {
         
     }
     
     void push(int x) {
+        
+        q3.push(x);
+        
+        int n = q3.size();
+        
+        while(n > 1)
+        {
+            int front = q3.front();
+            q3.pop();
+            
+            q3.push(front);
+            n--;
+        }
+        /*
+        
+            ------------------IMPLEMENTATION - 1----------------------------------
+                                   2 QUEUES
+            
         q2.push(x);
         
         while(!q1.empty())
@@ -36,24 +70,36 @@ public:
         }
         
         swap(q1 , q2);
-    }
     
+       */
+    }
     int pop() {
         
+         int temp  = q3.front();
+        
+        q3.pop();
+        
+        return temp;
+        
+        /*
+        ------------------IMPLEMENTATION - 1----------------------------------
+                                   2 QUEUES
         int temp  = q1.front();
         
         q1.pop();
         
         return temp;
+        
+        */    
     }
     
     int top() {
-        return q1.front();
+        return q3.front();
     }
     
     bool empty() {
         
-        return q1.empty() == true ? true : false;
+        return q3.empty() == true ? true : false;
     }
 };
 
