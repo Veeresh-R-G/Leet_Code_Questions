08e000cdb1564ec3a40e8b0359804030f4b2010b
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-07-10 12:44:35 +0530

    Time: 30 ms (5.26%), Space: 13.9 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -3,10 +3,13 @@ public:
     
     int solve(vector<int>&cost , int ind , int N , vector<int>& dp)
     {
+        
+        //Base Case
         if(ind == N - 1)
         {
             return cost[ind];
         }
+        
         if(dp[ind] != -1)
         {
             return dp[ind];
@@ -26,11 +29,29 @@ public:
         
         int n = cost.size();
         
-        vector<int> dp(n + 1 , -1);
-        int zero = solve(cost , 0 , n , dp);
-        int one = solve(cost , 1 , n, dp);
+        vector<int> dp(n + 1, 0);
         
-        return min(zero , one);
+        
+        //Base Case
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i = 2; i <n ; i++)
+        {
+            dp[i] = min(cost[i] + dp[i - 1] , cost[i] + dp[i - 2]);
+        }
+        for(auto it : dp)
+        {
+            cout << it << " ";
+        }
+        return min(dp[n -1] , dp[n - 2]);
+        
+        
+        
+//         int zero = solve(cost , 0 , n , dp);
+//         int one = solve(cost , 1 , n, dp);
+        
+//         return min(zero , one);
         
     }
 };
