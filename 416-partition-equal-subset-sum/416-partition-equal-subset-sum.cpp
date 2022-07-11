1a8e61297079862f66ae7a078bc1e5e5486343a2
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-07-11 21:20:12 +0530

    Time: 703 ms (9.55%), Space: 75.3 MB (30.02%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,7 +3,7 @@ public:
     
     bool solve(vector<int>& nums , int ind , int target ,vector<vector<int>> &dp)
     {
-        if(target < 0) return false;
+       
         if(target == 0)
         {
             return true;
@@ -26,6 +26,10 @@ public:
     
     bool canPartition(vector<int>& nums) {
         
+        if(nums.size() == 1)
+        {
+            return false;
+        }
         int N = nums.size();
         int sum = 0;
         for(auto ele : nums) sum += ele;
@@ -35,7 +39,41 @@ public:
             return false;
         }
         
+        /*
+        This is for MemoiZation
+        
         vector<vector<int>> dp(N , vector<int>((sum / 2) + 2 , -1));
-        return solve(nums , N - 1 , (sum / 2) , dp); 
+        */
+        
+        //Tabulation
+        
+        vector<vector<int>> dp(N , vector<int>((sum / 2) + 1 , 0));
+        
+        //Base Case - 1
+        for(int ind = 0 ; ind < N ; ind++)
+        {
+            dp[ind][0] = true;
+        }
+        
+        //Base Case - 2
+        if(nums[0] <= (sum / 2))
+        dp[0][nums[0]] = true;
+        
+        
+        for(int ind = 1 ; ind < N ; ind++)
+        {
+            for(int target = 1 ; target <= (sum / 2) ; target++)
+            {
+                
+                bool pick = nums[ind] <= target ? dp[ind - 1][target - nums[ind]] :false;
+        
+        bool notPick = dp[ind - 1][target];
+                
+                dp[ind][target] = pick || notPick;
+            }
+        }
+        
+        return dp[N - 1][sum / 2];
+        // return solve(nums , N - 1 , (sum / 2) , dp); 
     }
 };
