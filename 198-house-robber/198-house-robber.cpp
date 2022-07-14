0ebd9d12aad2ea98309b94e7e9377b4dc180b1c9
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-07-14 12:43:22 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -23,7 +23,28 @@ public:
         
         int n = nums.size();
         
-        vector<int> dp(n , - 1);
-        return solve(nums , n - 1 , dp);
+        /* 
+            vector<int> dp(n , - 1);  -----> MemoIzation 
+        */
+        
+        //Tabulation
+        vector<int> dp( n , 0);
+        
+        //Base Case
+        dp[0] = nums[0];
+        
+        for(int ind = 1 ; ind < n ; ind++)
+        {
+             int pick =ind > 1 ?  nums[ind] + dp[ind - 2] : nums[ind];
+             int notPick = dp[ind - 1];
+            
+            dp[ind] = max(pick , notPick);
+        }
+        
+        return dp[n - 1];
+        /*
+            return solve(nums , n - 1 , dp); ---> MemoIzation
+        */
+        
     }
 };
