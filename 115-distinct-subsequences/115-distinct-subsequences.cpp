46b83269daef61dc0249933fb9cdaac4d1d704bb
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-07-17 00:46:55 +0530

    Time: 41 ms (76.80%), Space: 30.2 MB (17.06%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -33,14 +33,14 @@ public:
            prev[0] = curr[0]= 1;
         for(int i = 1 ; i <= n ; i++)
         {
-            for(int j = 1; j<=m ; j++)
+            for(int j = m; j >= 1 ; j--)
             {
-                if(s[i - 1] == t[j - 1]) curr[j] = prev[j - 1] + prev[j];
+                if(s[i - 1] == t[j - 1]) prev[j] = prev[j - 1] + prev[j];
                 
-                else curr[j] = prev[j];
+                else prev[j] = prev[j];
             }
             
-            prev = curr;
+            
         }
         
         return (int)prev[m];
