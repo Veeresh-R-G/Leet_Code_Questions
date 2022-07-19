8149738dc06f6ad6ac2e12fbed2ec7f29d40d6f1
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-07-19 10:18:03 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (38.38%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -7,14 +7,14 @@ public:
     int solve(string& s , string& t , int i , int j,vector<vector<int>>& dp)
     {
         //Base-Cases
-        if(i < 0 && j < 0) return 0;
+        if(i == 0 && j == 0) return 0;
         
-        if(i < 0 && j >= 0) return j + 1; //Insertions
+        if(i == 0 && j > 0) return j ; //Insertions
         
-        if(j < 0 && i >= 0) return i + 1; // Deletions
+        if(j == 0 && i > 0) return i ; // Deletions
         
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j])
+        if(s[i - 1] == t[j - 1])
         {
             return dp[i][j] =solve(s, t , i - 1 , j - 1 , dp);
         }
@@ -32,7 +32,7 @@ public:
         int n = word1.size();
         int m = word2.size();
         
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-        return solve(word1 , word2 , n - 1,  m - 1 , dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1 , -1));
+        return solve(word1 , word2 , n ,  m , dp);
     }
 };
