c8409e036bafd9e30daced3a9d8c02e1be0dde6c
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-09-25 10:29:21 +0530

    Time: 6 ms (6.12%), Space: 6.3 MB (16.00%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
     
-    /*
-    
-        Ok this is a DP problem
-        
-    */
-    
-    //recursive
-    int ways(int n)
+    int f(int n , vector<int>& dp)
     {
-        if(n == 1) return 1;
-        if(n == 0) return 1;
-        
-        int one = ways(n -1);
-        int two = ways(n - 2);
-        
-        return one + two;
-        
-    }
-    
-    
-    //Memoization
-    int ways_memo(int n , vector<int>& dp)
-    {
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-        
-        if(dp[n] != -1)
+        if(n == 0) 
         {
-            return dp[n];
+            return 1;
         }
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
         
-        else
-        {
-            return dp[n] = ways_memo(n - 1 , dp) + ways_memo(n - 2 , dp);
-        }
+        return dp[n] = f(n -1 , dp) + f(n - 2 , dp); 
     }
-    
     int climbStairs(int n) {
-    
-        vector<int> dp(n + 1 , -1);
-        return ways_memo(n , dp);
-        // return ways(n);
         
+        vector<int> dp( n + 1 , 0);
+        
+        dp[0] = 1;
+        
+        for(int i = 1 ; i <= n ; i++)
+        {
+            int one = i >= 1 ? dp[i - 1] :0;
+            
+            int two = i >= 2 ? dp[i - 2] :0;
+            
+            dp[i] = one + two;
+        }
+        
+        return dp[n];
+        // return f(n , dp);
     }
 };
