9ee602b51c74a6d294126516092f3b5d7d0067d1
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-10-04 12:03:42 +0530

    Time: 52 ms (21.42%), Space: 14.4 MB (12.07%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(vector<int>& prices ,vector<vector<vector<int>>> &dp , int ind , int buy , int trans)
+    {
+        if (ind >= prices.size() || trans <= 0) return 0;
+        
+        if(dp[ind][buy][trans] != -1) return dp[ind][buy][trans];
+        if(buy)
+        {
+            int BUY = -prices[ind] + solve(prices , dp , ind + 1 , 0 , trans);
+            int NBUY = solve(prices , dp , ind + 1 , 1 , trans);
+            
+            return dp[ind][buy][trans] = max(BUY , NBUY);
+        }
+        
+        return dp[ind][buy][trans] = max(+prices[ind] + solve(prices , dp , ind + 1, 1 , trans - 1) ,
+                                 solve(prices , dp , ind + 1, 0 , trans));
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        
+         int n = prices.size();
+        
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2 , vector<int>(k + 1 , -1)));
+        return solve(prices , dp , 0 , 1 , k);
+    }
+};
