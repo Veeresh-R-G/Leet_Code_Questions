871b28362a28576491a5da2f564d543e2d501a3a
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2022-12-17 09:45:51 +0530

    Time: 11 ms (89.14%), Space: 12.2 MB (52.63%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    
+    bool isOperator(string op)
+    {
+        return op == "*" || op == "/" || op == "+" || op == "-";
+    }
+    
+    int evalRPN(vector<string>& tokens) {
+        
+        int n = tokens.size();
+        
+        if(n == 1)
+        {
+            string ans = tokens[0];
+            return stoi(ans);
+        }
+        
+        long long ans = 0;
+        
+        stack<long long> st;        
+        for(int i = 0 ; i < n ; i++)
+        {
+            string ele = tokens[i];
+            
+            if(isOperator(ele))
+            {
+                long long num1 = st.top();
+                st.pop();
+                long long num2 = st.top();
+                st.pop();
+                if(ele == "-")
+                {
+                    ans = (num2 - num1);
+                }
+                else if(ele == "*")
+                {
+                    ans = (num2 * num1);
+                }
+                else if(ele == "/")
+                {
+                    ans = (num2 / num1);
+                }
+                else
+                {
+                    ans = (num2 + num1);
+                }
+                
+                st.push((ans));
+            }
+            else
+            {
+                st.push(stoi(ele));
+            }
+        }
+        
+        
+        return ans;
+        
+    }
+};
