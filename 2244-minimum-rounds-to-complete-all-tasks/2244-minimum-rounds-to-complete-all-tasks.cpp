592de9c585ffb171a88ed55c3ad59b009c71ba50
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-01-04 07:00:50 +0530

    Logic Update : 2244-minimum-rounds-to-complete-all-tasks

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    /*
+                ----LOGIC----
+                
+                If we have freq % 3 == 0 we require (freq / 3) rounds to finish all the tasks
+                
+                else
+                    For any case it will be (freq / 3) + 1
+                    
+                    
+                    
+                 let the number be of any form like : (3k+1) / (3k + 2)
+                 
+                 still the rounds will be (freq / 3) + 1
+                 
+                 For example : 
+                 10 --> 3 + 3 + 2 + 2 == 4 rounds == (10 / 3) + 1 ==> 3 + 1 => 4
+                 
+                 11 ---> 3 + 3 + 3 + 2 so its still the same
+                
+                
+    */
     int minimumRounds(vector<int>& tasks) {
         
         map<int , int> m;
