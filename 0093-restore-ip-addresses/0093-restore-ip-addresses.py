8cdd2b351933f93b9bbbef7b819efdc130970ba9
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-01-21 13:33:15 +0530

    Time: 38 ms (77.95%), Space: 14 MB (31.50%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,6 +1,9 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         
+        '''
+            This is Invalid bcoz IP length can never be above 12
+        '''
         if(len(s) > 12):
             return []
         
@@ -16,6 +19,15 @@ class Solution:
                 return
             
             
+            '''
+                s[ind : i + 1] --> Value check
+                
+                ind == i --> Single digit (0 , 1 , 2, 3, .. 9)
+                
+                s[ind] != "0" ---> For multiple digits but 
+                                   starting digit should not be 0(no trailing 0)
+                
+            '''
             for i in range(ind , min(ind + 3 , len(s))):
                 if int(s[ind : i + 1]) <= 255 and (ind == i or s[ind] != "0"):
                     f(i + 1 , currIp + s[ind : i + 1] + "." , dots + 1)
