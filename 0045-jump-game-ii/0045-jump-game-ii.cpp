bde43924929abfc7ac1d90c99036370d3aac6eae
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-02-08 07:04:33 +0530

    Time: 511 ms (21.24%), Space: 18.1 MB (9.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     
-    int f(vector<int>& nums ,int ind , int n , vector<int>& dp)
+    int f(vector<int>& nums , int ind , int N , vector<int>& dp)
     {
-        if(ind >= n - 1)
+        if(ind >= N - 1)
         {
             return 0;
         }
         
-        if(dp[ind] != -1) return dp[ind];
-        
-        int miniJumps = 1e9;
-        
+        if(dp[ind] != -1)
+        {
+            return dp[ind];
+        }
+        int mini = 1e7;
         for(int i = 1 ; i <= nums[ind] ; i++)
         {
-            miniJumps = min(miniJumps , 1 + f(nums , ind + i , n , dp));
+            
+            mini = min(mini ,1 + f(nums , ind + i , N, dp));
         }
         
-        return dp[ind] = miniJumps;
+        return dp[ind] = mini;
     }
     
     int jump(vector<int>& nums) {
-        
-        
-        int n = nums.size();
+     
+        int N = nums.size();
         int ind = 0;
-        vector<int> dp(n + 1 , -1);
-        return f(nums , ind , n ,dp);
+        vector<int> dp(N , -1);
+        return f(nums , ind , N , dp);
     }
 };
