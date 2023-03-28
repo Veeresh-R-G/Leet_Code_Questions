4e1a515a34575824394b59d49e74a75773e15f1c
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-03-28 22:30:13 +0530

    Time: 751 ms (18.81%), Space: 155.2 MB (33.27%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    /*
+        
+    */
+    int MOD = 1e9 + 7;
+    
+    int f(vector<vector<int>>& grid, int k , int row , int col , int rowLim , int colLim,
+         int total , vector<vector<vector<int>>>& dp)
+    {
+        
+        if(row >= rowLim || col >= colLim)
+        {
+            return 0;
+        }
+        if(row == rowLim - 1 && col == colLim - 1)
+        {
+            return (grid[row][col] + total) % k == 0;
+        }
+        
+        
+        if(dp[row][col][total % k] != -1) return dp[row][col][total % k];
+        
+        // total += grid[row][col];
+        
+        int right = f(grid , k , row  , col + 1 , rowLim , colLim ,total + grid[row][col] , dp) % MOD;
+        int down = f(grid , k , row + 1 , col , rowLim , colLim , total + grid[row][col]  , dp) % MOD;
+        
+        return dp[row][col][total % k] = ((right % MOD) + (down % MOD)) % MOD;
+        
+        
+    }
+    
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+                
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = 0;
+        
+        vector<vector<vector<int>>> dp(m + 1 , vector<vector<int>>(n + 1 , vector<int>(k , -1)));
+        
+        return f(grid , k , 0 , 0 , m , n , total , dp); 
+    }
+};
