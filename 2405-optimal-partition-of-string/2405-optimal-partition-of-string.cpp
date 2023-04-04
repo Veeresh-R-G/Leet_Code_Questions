eba3d8a69aef1282ebe039288cb1a23a6870b51e
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-04-04 19:16:38 +0530

    Time: 31 ms (81.17%), Space: 10.1 MB (98.68%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        
+        
+        /*
+        
+            Two pointer will not work : 
+            aabbc
+            will just compare the first character with the substring that I am checking
+            Will not take care of the inner characters
+            
+            
+        */
+        
+        vector<int> seen(26 , -1);
+        
+        int cnt = 1;
+        int substringStart = 0;
+        
+        int N = s.size();
+        
+        
+        for(int i = 0 ; i < N ; i++)
+        {
+            if(seen[s[i] - 'a'] >= substringStart)
+            {
+                cnt++;
+                substringStart = i;
+            }
+            seen[s[i] - 'a'] = i;
+        }
+        
+        return cnt;
+    }
+};
