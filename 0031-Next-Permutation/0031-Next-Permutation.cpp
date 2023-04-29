542c2b592642f0450cb42990703286e432fd83dc
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-04-29 20:43:30 +0530

    Runtime: 3 ms, faster than 82.91% of C++ online submissions for Next Permutation. Memory Usage: 12 MB, less than 76.09% of C++ online submissions for Next Permutation.

---
+++ 0031-Next-Permutation/0031-Next-Permutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        // next_permutation(nums.begin() , nums.end());
+        
+        int ind = -1;
+        int n = nums.size();
+        
+        for(int i = n - 2 ; i >= 0 ; i--)
+        {
+            if(nums[i] < nums[i + 1])
+            {
+                ind = i;
+                break;
+            }
+        }
+        
+        if(ind == -1)
+        {
+            //Last permutation
+            reverse(nums.begin() , nums.end());
+            return;
+        }
+        
+        for(int i = n - 1 ; i >= ind ; i--)
+        {
+            if(nums[i] > nums[ind])
+            {
+                swap(nums[i] ,nums[ind]);
+                break;
+            }
+        }
+        
+        reverse(nums.begin() + ind + 1 , nums.end());
+    }
+};
