edfa0f3032429bba448cf698ba9fde2487805f9e
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-05-08 20:46:46 +0530

    Time: 17 ms (48.72%), Space: 11.2 MB (64.67%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+     
+        int N = mat.size();
+        int mid = N / 2;
+        
+        
+        int sum = 0;
+        
+        
+        //Lol no need of two loops nice..Good self - realisation
+        for(int i = 0 ; i < N ; i++)
+        {
+            sum += (mat[i][i] + mat[N - 1 - i][i]);
+            cout << sum << " ";
+        }
+        
+        sum = N % 2 ? sum - mat[mid][mid] : sum;
+        
+        
+        return sum;
+    }
+};
