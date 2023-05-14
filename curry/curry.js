1c38cb9a8d1fe9b1a09bf2fb3f73936694813d2c
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-05-14 22:07:37 +0530

    Time: 76 ms (29.78%) | Memory: 48.4 MB (49.89%) - LeetSync

---
+++ curry/curry.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+   return function curried(...args) {
+      if(args.length >= fn.length) {
+          console.log(fn)
+;         return fn(...args);
+      }
+
+      return (...nextArgs) => curried(...args, ...nextArgs);
+   };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
