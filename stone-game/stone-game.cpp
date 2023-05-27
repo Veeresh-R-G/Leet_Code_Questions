80abed2025e8bd8a8871662943e121750f648706
Veeresh <72454785+Veeresh-R-G@users.noreply.github.com>
2023-05-27 19:10:49 +0530

    Time: 679 ms (5.06%) | Memory: 69.4 MB (5.85%) - LeetSync

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -1,7 +1,48 @@
 class Solution {
+
+private:
+
+    map<pair<int, int> , int> mp;
+
 public:
+
+
+    int f(vector<int>& piles , int l , int r)
+    {
+        if(l > r)
+        {
+            return 0;
+        }
+
+        if(mp.find({l , r}) != mp.end())
+        {
+            return mp[{l , r}];
+        }
+
+        //Even length is only when Alice is picking
+        //But on zero based indexing (r - l) will be odd when len of array will be even
+        //Even --> ALice is making choice
+        //Odd --> Bob is picking
+        bool even = (r - l) % 2 ? true : false;
+        
+        int left = even ? piles[l] : 0;
+        int right = even ? piles[r] : 0;
+
+        
+        return mp[{l , r}] = max(f(piles , l + 1 , r) + left , 
+        f(piles , l , r - 1) + right);
+
+    }
+
     bool stoneGame(vector<int>& piles) {
         
-        return true;
+        int l = 0;
+        int r = piles.size();
+
+        int sum = 0;
+        for(auto it : piles) sum += it;
+
+        return f(piles , l , r - 1) > (sum / 2);
+        
     }
 };
